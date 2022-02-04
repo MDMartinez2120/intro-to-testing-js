32f4e6a3f3585356ccecc0c5ecece9d77633d772
Matthew Martinez <matt.martinez2120@gmail.com>
2022-02-04 16:43:23 -0600

    adding js test files

--- code.js
+++ code.js
@@ -1,4 +1,26 @@
-// helloWorld function 
-function helloWorld() {
+// // helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+const helloWorld = function() {
     return "Hello, World!";
 }
+
+const sayHello = function() {
+    return "Hello, World!";
+}
+
+// describe('helloWorld', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof helloWorld).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//         expect(typeof helloWorld()).toBe("string");
+//     });
+//     it('should return the string "Hello, World!" when executed', function() {
+//         expect(helloWorld()).toBe("Hello, World!");
+//     });
+//     it("should never return 'undefined' when called", function() {
+//         expect(helloWorld()).not.toBe(undefined);
+//     });
+// });
